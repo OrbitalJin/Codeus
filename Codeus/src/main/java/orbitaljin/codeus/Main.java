2ca7f0d2c6c3960d60e37c283569b0f656679539
HorizonLabs <44175745+QuaeriteVeritatis@users.noreply.github.com>
2024-04-24 17:50:38 +0100

    API Setup

--- Codeus/src/main/java/orbitaljin/codeus/Main.java
+++ Codeus/src/main/java/orbitaljin/codeus/Main.java
@@ -1,10 +1,13 @@
 package orbitaljin.codeus;
 
+import orbitaljin.codeus.api.APIController;
 import orbitaljin.codeus.store.DBHandler;
 import orbitaljin.codeus.store.models.Book;
 
+import org.springframework.boot.SpringApplication;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         DBHandler.getInstance().bookRepository.create(
             new Book(
                     "The Great Gatsby",
@@ -13,5 +16,6 @@ public class Main {
         System.out.println(
                 DBHandler.getInstance().bookRepository.getAll()
         );
+        SpringApplication.run(APIController.class, args);
     }
 }
---
+++ Codeus/src/main/java/orbitaljin/codeus/api/APIController.java
@@ -0,0 +1,14 @@
+package orbitaljin.codeus.api;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class APIController {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+}
